e674ec2600dbb355475e377876ae7d2a8d50f8bc
codercodemanish <72806831+codercodemanish@users.noreply.github.com>
2022-03-02 18:24:51 +0530

    test

--- js/main.js
+++ js/main.js
@@ -1,18 +1,5 @@
- //    owl-crousel for blog
-
-
-
+//    owl-crousel for blog
 $(document).ready(function(){
-
-  // $('.owl-carousel').owlCarousel({
-  //   loop:true,
-  //   autoplay:true,
-  //   autoplayTimeout:3000,
-  //   dots:false,
-  //   nav:true,
-  //   navText:[$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
-  //   responsive: responsive
-  // });
 
   $('.owl-carousel').owlCarousel({
     loop:true,
@@ -33,33 +20,8 @@ $(document).ready(function(){
         }
     }
   });
+
+  AOS.init();
+
 });
 
-
-
-
-
-// $(document).ready(function(){
-//   //  $nav = $('.nav');
-//   //  $toggleCollapse = $('.toggle-collapse')
-
-//   //  click event on toggle menu
-//   //  $toggleCollapse.click(function (){
-//       //  alert('hello');
-//       //  $nav.toggleClass('collapse');
-//   //  })
-
-
-//   // click to scrole top
-//   $('.move-up span').click(function(){
-//     $('html,body').animate({
-//       scrollTop:0
-//     },1000);
-//   })
-
-//   //  AOS Animation
-//   AOS.init();
-
-
-
-// });
