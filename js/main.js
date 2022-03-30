6df6bc95e1726a1eea493e61beae1f845101acda
codercodemanish <72806831+codercodemanish@users.noreply.github.com>
2022-03-30 11:20:31 +0530

    card hieght change anagan

--- js/main.js
+++ js/main.js
@@ -24,17 +24,63 @@ $(document).ready(function(){
   AOS.init();
 
   
+  // objective 1
   $('#read-more1').click(function(){
-    $('.more1').toggle('show');
-  })
+      let key = $('#read-more1').text();
+      console.log(key);
+      if(key == '.... Read more'){
+        $('.more1').show(1000,function(){
+          $('#read-more1').text('.... Read less')
+        });
+      }else{
+        $('.more1').hide(1000,function(){
+          $('#read-more1').text('.... Read more');
+        });
+      }
+  });
 
+  // objective 2
   $('#read-more2').click(function(){
-    $('.more2').toggle('show');
-  })
+    let key = $('#read-more2').text();
+    console.log(key);
+    if(key == '.... Read more'){
+      $('.more2').show(1000,function(){
+        $('#read-more2').text('.... Read less')
+      });
+    }else{
+      $('.more2').hide(1000,function(){
+        $('#read-more2').text('.... Read more');
+      });
+    }
+});
 
-  $('#read-more3').click(function(){
-    $('.more3').toggle('show');
-  })
+// objective 3
+$('#read-more3').click(function(){
+  let key = $('#read-more3').text();
+  console.log(key);
+  if(key == '.... Read more'){
+    $('.more3').show(1000,function(){
+      $('#read-more3').text('.... Read less')
+    });
+  }else{
+    $('.more3').hide(1000,function(){
+      $('#read-more3').text('.... Read more');
+    });
+  }
+});
+
+  // $('#read-more1').click(function(){
+  //     let key = $('#read-more1').text();
+  //   $('.more1').toggle('show');
+  // })
+
+  // $('#read-more2').click(function(){
+  //   $('.more2').toggle('show');
+  // })
+
+  // $('#read-more3').click(function(){
+  //   $('.more3').toggle('show');
+  // })
 
 });
 
